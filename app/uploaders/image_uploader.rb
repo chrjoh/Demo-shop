e0ef17866cdf4ca6e512d2e2d9776c9fe4ec599e
chrjoh <christerjoha@gmail.com>
2011-03-22 15:07:04 +0100

    css and link fixes for image upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,10 +3,10 @@ require 'carrierwave/processing/mini_magick'
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  process :resize_to_fit => [200, 200]
+  process :resize_to_fit => [290, 70]
 
   version :thumb do
-    process :resize_to_fill => [80, 80]
+    process :resize_to_fill => [80, 40]
   end
 end
         
